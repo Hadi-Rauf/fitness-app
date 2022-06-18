5d629755c34fe7c26b47423ccebc21e325594f2d
Hadi-Rauf <mohammadhadirauf@gmail.com>
2022-06-18 21:06:34 +0500

    done

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <Box sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}>
+      <Typography color='#FF2625' fontWeight='600' fontSize='30px'>
+        Fitness club
+      </Typography>
+    </Box>
+  );
 };
 
 export default HeroBanner;
